f682e9f487d3a6fefebf1d1e892f51f28184f606
Gordan Glavaš <gordan.glavas@codecons.com>
2018-08-05 16:34:37 +0200

    Implemented queries, path parsing, and custom code responses

--- web/src/main/java/net/globulus/simisync/web/SimiSyncController.java
+++ web/src/main/java/net/globulus/simisync/web/SimiSyncController.java
@@ -2,12 +2,17 @@ package net.globulus.simisync.web;
 
 import net.globulus.simi.ActiveSimi;
 import net.globulus.simi.SimiMapper;
+import net.globulus.simi.api.SimiObject;
+import net.globulus.simi.api.SimiProperty;
 import net.globulus.simi.api.SimiValue;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.HandlerMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.HashMap;
@@ -17,7 +22,7 @@ import java.util.stream.Collectors;
 public class SimiSyncController {
 
     @RequestMapping("/**")
-    public String route(HttpServletRequest request) {
+    public ResponseEntity<String> route(HttpServletRequest request, HttpServletResponse response) {
         String body = null;
         if (request.getMethod().equalsIgnoreCase("post")) {
             try {
@@ -31,11 +36,25 @@ public class SimiSyncController {
         for (String header : Collections.list(request.getHeaderNames())) {
             headers.put(header, request.getHeader(header));
         }
-        return "" + ActiveSimi.eval("SimiSyncControllers.Router",
+        String queryString = request.getQueryString();
+        SimiProperty prop = ActiveSimi.eval("SimiSyncControllers.Router",
                 "route",
                 new SimiValue.String(endpoint),
                 new SimiValue.String(request.getMethod()),
                 new SimiValue.Object(SimiMapper.toObject(headers, true)),
-                new SimiValue.String(body));
+                (queryString == null) ? null : new SimiValue.String(request.getQueryString()),
+                (body == null) ? null : new SimiValue.String(body));
+        if (prop == null) {
+            return ResponseEntity.notFound().build();
+        }
+        SimiValue value = prop.getValue();
+        if (value instanceof SimiValue.String) {
+            return ResponseEntity.ok(value.getString());
+        } else {
+            SimiObject obj = value.getObject();
+            int statusCode = (int) obj.get("code", null).getValue().getNumber().doubleValue();
+            String responseBody = obj.get("body", null).getValue().getString();
+            return ResponseEntity.status(statusCode).body(responseBody);
+        }
     }
 }

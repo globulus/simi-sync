b596d06ba215f16039c5ee44fb40e46ef4ecb7f1
Gordan Glavaš <gordan.glavas@codecons.com>
2018-09-17 13:14:41 +0200

    Updates based on latest interpreter version

--- web/src/main/java/net/globulus/simisync/SimiSyncApplication.java
+++ web/src/main/java/net/globulus/simisync/SimiSyncApplication.java
@@ -22,7 +22,7 @@ public class SimiSyncApplication {
 
 	public static void main(String[] args) {
 		ResourceLoaderWrapper resourceLoaderWrapper = new ResourceLoaderWrapper(resourceLoader);
-		ActiveSimi.setDebugMode(true);
+//		ActiveSimi.setDebugMode(true);
 		ActiveSimi.setImportResolver(new ActiveSimi.ImportResolver() {
 			@Override
 			public String readFile(String s) {
